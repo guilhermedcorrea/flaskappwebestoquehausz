7ad9e215814468723a4ef078ddc12014b94fe124
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2022-08-23 13:50:07 -0300

    Update reader_csv_file.py

--- app/admin/reader_csv_file.py
+++ app/admin/reader_csv_file.py
@@ -3,11 +3,8 @@ from sqlalchemy import text
 from flask_sqlalchemy import SQLAlchemy
 
 
-
-from ..controllers.wraps_functolls import (verify_group_users_hausz_mapa
-, create_log_operations,create_log_operations, call_procedure_saldo, call_procedure_prazos)
-
-
+from ..controllers.wraps_functolls import (
+    verify_group_users_hausz_mapa, create_log_operations, create_log_operations, call_procedure_saldo, call_procedure_prazos)
 
 
 db = SQLAlchemy()
@@ -18,136 +15,144 @@ def configure(app):
     app.db = db
 
 
+def get_id_marca(ref):
+    with db.engine.begin() as conn:
+        exec = (text("""SELECT  IdMarca
+            FROM [HauszMapa].[Produtos].[ProdutoBasico]
+            WHERE SKU='{}'""".format(ref)))
+        try:
+            exec_produtos = conn.execute(exec).first()
+            idmarca = str(exec_produtos[0])
+     
+            yield idmarca
+        except:
+            print("notofund")
+     
+   
 @verify_group_users_hausz_mapa
-def get_id_user(*args, **kwars):
-    
+def get_id_user(*args, **kwargs):
+
     pass
 
+@create_log_operations
 @call_procedure_prazos
-def update_prazos_skus(*args, **kwarsg):
-    pass
-
-@call_procedure_saldo
-def update_saldos_skus(*args, **kwargs):
-
-    try:
-
-        with db.engine.begin() as conn:
-
-            exec = (text(
-                    """EXEC Estoque.SP_AtualizaEstoqueFornecedor @Quantidade = {}, @CodigoProduto = '{}', @IdMarca = {}""".format(
-                        args[0], args[1], args[2])))
-            exec_produtos = conn.execute(exec)
-
-
-           
-    except:
-            print('erro')
-
+def update_prazos_skus(*args, **kwargs):
+    print(type(kwargs))
+    print("INSERINDO PRAZO", kwargs.get('ref'), kwargs('pz'), kwargs.get('brand'))
 
 
 @create_log_operations
-def get_produtos(*args, **kwargs):
-    with db.engine.connect() as conn:
-        exec = (text("""
-            SELECT basico.[IdProduto],basico.[SKU],basico.[NomeProduto] ,basico.[IdMarca]
-            FROM [HauszMapa].[Produtos].[ProdutoBasico] as basico
-            JOIN [HauszMapa].[Produtos].[Marca] as brand
-            ON brand.IdMarca = basico.IdMarca
-            WHERE basico.SKU = '{}'""".format(args[0])))
-        exec_sku_produto = conn.execute(exec).all()
-        for produto in exec_sku_produto:
-            try:
-                dict_produtos = {
-                    'SKU':produto['SKU'],
-                    'NomeProduto':produto['NomeProduto'],
-                    'IDMARCA':produto['IdMarca']}
+@call_procedure_saldo
+def update_saldos_skus(*args, **kwargs):
+    print(type(kwargs))
+    print("INSERINDO SALDOS", kwargs.get('ref'), kwargs.get('sd'),kwargs.get('brand'))
+ 
 
-                yield dict_produtos
-            except:
-                print("error teste")
-                
-            
-        
+def get_produtos(*args, **kwargs):
+    if isinstance(kwargs, dict):
+        idmarca = list(get_id_marca(kwargs.get('ref')))
+        print("criando logs", kwargs)
+        print(kwargs.get('ref'), kwargs.get('sd'), idmarca = idmarca)
+
+    if isinstance(args, list):
+        print("criando logs")
+      
+        print(kwargs.get('ref'), kwargs.get('sd'), idmarca = idmarca)
+
 
 class ReaderCsv:
     def __init__(self, path):
         self.path = path
         self.dict_rows = {}
-
     def reader_csv(self, file):
-  
         with open(file, newline='') as csvfile:
-
             reader = csv.DictReader(
                 csvfile, delimiter=";", skipinitialspace=True)
             for row in reader:
-        
+                try:
+                    row['IDMARCA'] = list(get_id_marca(row['SKU']))[0]
+                    print('branddd',row['IDMARCA'])
+                except Exception as e:
+                    print(e,'erroidmarca')
+
                 try:
 
-                    if len(row['PRAZO']) == 0 and len(row['SALDO']) !=0:
+                    if len(row['PRAZO']) == 0 and len(row['SALDO']) != 0:
                         sku = str(row['SKU'].strip())
                         try:
-                            saldo = str(row['SALDO'].replace(".","").replace(",",".").strip())
+                            saldo = str(row['SALDO'].replace(
+                                ".", "").replace(",", ".").strip())
+                            saldo = float(saldo)
+
+                        except:
+                            pass
+                        try:
+                            prazo = str(row['PRAZO'].replace(
+                                ".", "").replace(",", ".").strip())
+                            prazo = float(prazo)
+                        except:
+                            pass
+                        
+                        try:
+                            idmarca = int(row['IDMARCA'])
+                       
+                        except:
+                            pass
+                
+                        #list(get_produtos(ref=sku, sd=saldo, brand=idmarca))
+                        list(update_saldos_skus(ref=sku, sd=saldo, brand=idmarca))
+                    
+
+                    elif len(row['SALDO']) == 0 and len(row['PRAZO']) != 0:
+                        sku = str(row['SKU'].strip())
+                        try:
+                            saldo = str(row['SALDO'].replace(
+                                ".", "").replace(",", ".").strip())
                             saldo = float(saldo)
                         except:
                             pass
                         try:
-                            prazo = str(row['PRAZO'].replace(".","").replace(",",".").strip())
+                            prazo = str(row['PRAZO'].replace(
+                                ".", "").replace(",", ".").strip())
                             prazo = float(prazo)
                         except:
                             pass
-                        idmarca = str(row['IDMARCA'].strip())
-                        idmarca = int(idmarca)
-
-                     
-                        dicts = list(get_produtos(sku))
-                        print(dicts)
-                      
-                        #select_hausz_mapa(row['SKU'], row['SALDO'], row['IDMARCA'], row['PRAZO'])
-
-                    elif len(row['SALDO']) == 0 and len(row['PRAZO']) !=0:
-                        sku = str(row['SKU'].strip())
                         try:
-                            saldo = str(row['SALDO'].replace(".","").replace(",",".").strip())
-                            saldo = float(saldo)
+                            idmarca = int(row['IDMARCA'])
+                           
                         except:
                             pass
-                        try:
-                            prazo = str(row['PRAZO'].replace(".","").replace(",",".").strip())
-                            prazo = float(prazo)
-                        except:
-                            pass
-                        idmarca = str(row['IDMARCA'].strip())
-                        idmarca = int(idmarca)
 
-              
-                        dicts = list(get_produtos(sku))
-                        print(dicts)
+                        list(update_prazos_skus(ref=sku, pz=prazo, brand=idmarca))
+                        
 
                     elif len(row['PRAZO']) != 0 and len(row['SALDO']) != 0:
                         sku = str(row['SKU'].strip())
                         try:
-                            saldo = str(row['SALDO'].replace(".","").replace(",",".").strip())
+                            saldo = str(row['SALDO'].replace(
+                                ".", "").replace(",", ".").strip())
                             saldo = float(saldo)
                         except:
                             pass
                         try:
-                            prazo = str(row['PRAZO'].replace(".","").replace(",",".").strip())
+                            prazo = str(row['PRAZO'].replace(
+                                ".", "").replace(",", ".").strip())
                             prazo = float(prazo)
                         except:
                             pass
-                        idmarca = str(row['IDMARCA'].strip())
-                        idmarca = int(idmarca)
-
-                        dicts = list(get_produtos(sku))
-                        print(dicts)
-                        
-                      
-                        print('opção3', sku, saldo, prazo, idmarca)
-
+                        try:
+                            idmarca = int(row['IDMARCA'])
+                        except:
+                            pass
+                       
+                        list(update_prazos_skus(ref=sku, pz=prazo, brand=row['IDMARCA']))
                     else:
                         print('erro')
-                        print(row['SKU'], row['SALDO'], row['IDMARCA'], row['PRAZO'])
+                        idmarca = int(row['IDMARCA'])
+                        list(update_prazos_skus(ref=sku, pz=prazo, brand=idmarca))
+                        list(call_procedure_saldo(
+                            ref=sku, sd=saldo, brand=idmarca))
+                        print('opcao 4')
                 except:
-                    print("error campos nao encontrados")
+                    print(row['SKU'], row['SALDO'], row['PRAZO'])
+                  
