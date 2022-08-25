6e72329993f932238f70226d812f1111b064a890
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2022-08-25 15:06:46 -0300

    Update reader_csv_file.py

--- app/admin/reader_csv_file.py
+++ app/admin/reader_csv_file.py
@@ -20,7 +20,6 @@ def call_procedure_saldo_hausz_mapa(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
 
-        
         try:
             print('Calling procedure update SALDO', kwargs)
             with db.engine.begin() as conn:
@@ -36,6 +35,17 @@ def call_procedure_saldo_hausz_mapa(f):
             return f(*args, **kwargs)
     return wrapper
 
+'''
+
+
+      'sku': produtos_valores['SKU'],
+                    'saldo': somasaldo,
+                    'idmarca': produtos_valores['IDMARCA'],
+                    'idproduto': produtos_valores['IDPRODUTO'],
+                    'saldoanterior': produtos_valores['SALDOANTERIOR']
+                }
+
+'''
 
 def call_procedure_prazo_hausz_mapa(f):
     @wraps(f)
@@ -43,12 +53,12 @@ def call_procedure_prazo_hausz_mapa(f):
         print('Calling procedure update PRAZO', kwargs)
         try:
             with db.engine.begin() as conn:
-           
-                        exec = (text("""EXEC Estoque.SP_AtualizaPrazoProducao @Sku = '{}' ,@PrazoProducao = {}, @PrazoEstoqueFabrica """.format(
-                            str(kwargs.get('sku')),float(kwargs.get('prazo')), int(kwargs.get('idmarca')))))
 
-                        exec_produtos = conn.execute(exec)
-                        print("call procedure prazos - hausz_mapa_update_prazo",kwargs)
+                exec = (text("""EXEC Estoque.SP_AtualizaPrazoProducao @Sku = '{}' ,@PrazoProducao = {}, @PrazoEstoqueFabrica """.format(
+                    str(kwargs.get('sku')), float(kwargs.get('prazo')), int(kwargs.get('idmarca')))))
+
+                exec_produtos = conn.execute(exec)
+                print("call procedure prazos - hausz_mapa_update_prazo", kwargs)
         except:
             print("erro prazo")
             return f(*args, **kwargs)
@@ -75,50 +85,49 @@ def create_log_udapte_products(f):
             pass
         '''
 
-       
         return f(*args, **kwargs)
     return wrapper
+
 
 @create_log_udapte_products
 def get_updates_produtos(*args, **kwargs):
 
-    print('testeeeeeeeeee',kwargs)
+    print('testeeeeeeeeee', kwargs)
     try:
         with db.engine.begin() as conn:
             result = conn.execute(
                 insert(LogAlteracoesEstoques),
                 [
-                    {"idusuario": 1, "idprodutoalterado": kwargs.get('idproduto')},
+                    {"idusuario": 1, "idprodutoalterado": kwargs.get(
+                        'idproduto')},
                     {"idmarcaalterada": kwargs.get('idmarca'), "tipoalteracao": 1,
-                        "valoranterior":kwargs.get('saldoanterior'),"valoralterado":kwargs.get('saldo'),"dataalteracao":"2022-08-25", }
+                        "valoranterior": kwargs.get('saldoanterior'), "valoralterado": kwargs.get('saldo'), "dataalteracao": "2022-08-25", }
                 ]
             )
-            conn.commit()  
+            conn.commit()
 
     except:
-            pass
-       
-    print('criando log produtos',kwargs)
-   
+        pass
 
-  
+    print('criando log produtos', kwargs)
+
+
 @call_procedure_saldo_hausz_mapa
 def update_saldo_produtos(*args, **kwargs):
     """Recebe parametros de  entrada"""
     print("update saldo", kwargs)
-    get_updates_produtos(kwargs['saldo']
-        , kwargs.get('idmarca'), kwargs.get('idproduto'), kwargs.get('idmarca'), kwargs.get('saldoanterior'))
+    get_updates_produtos(kwargs.get('saldo'), kwargs.get('idmarca'), kwargs.get(
+        'idproduto'), kwargs.get('idmarca'), kwargs.get('saldoanterior'))
 
+    print('update saldo hausz produtos',
+          kwargs['sku'], kwargs['saldo'], kwargs['idmarca'])
 
-    
-    print('update saldo hausz produtos',kwargs['sku'], kwargs['saldo'], kwargs['idmarca'])
- 
 
 @call_procedure_prazo_hausz_mapa
 def update_prazo_produtos(*args, **kwargs):
     print('procedure prazo', kwargs)
 
-    print("call procedure prazos - hausz_mapa_update_prazo",kwargs)
+    print("call procedure prazos - hausz_mapa_update_prazo", kwargs)
 
 
 def get_prazos(*args, **kwargs):
@@ -128,15 +137,16 @@ def get_prazos(*args, **kwargs):
         if len(item.get('PRAZO')) > 0:
             prazo = int(item.get('PRAZO'))
 
-            get_updates_produtos(sku = item.get('SKU'), prazo = prazo
-            , idmarca = item.get('IDMARCA'), idproduto = item.get('IDPRODUTO'), prazoanterior = item.get('PRAZOANTERIOR'))
+            get_updates_produtos(sku=item.get('SKU'), prazo=prazo, idmarca=item.get(
+                'IDMARCA'), idproduto=item.get('IDPRODUTO'), prazoanterior=item.get('PRAZOANTERIOR'))
             prazolog = float(item.get('PRAZO'))
-            update_prazo_produtos(sku = item.get('SKU'), prazo = prazolog, idmarca = item.get('IDMARCA'))
+            update_prazo_produtos(sku=item.get(
+                'SKU'), prazo=prazolog, idmarca=item.get('IDMARCA'))
+
 
 def retorna_valores(ref):
-    #teste retorno Marca
- 
-   
+    # teste retorno Marca
+
     with db.engine.begin() as conn:
         exec = (text("""
             SELECT pfornecedor.PrazoProducao,brand.Marca
@@ -152,21 +162,22 @@ def retorna_valores(ref):
         exec_produto = conn.execute(exec).all()
         for produto in exec_produto:
             try:
-                dict_items={
-                    "sku":produto['SKU'],
-                    "idmarca":produto['IdMarca'],
-                    "idproduto":produto['IdProduto'],
-                    "saldo":produto['SaldoAtual'],
-                    "prazo":produto['PrazoProducao']
+                dict_items = {
+                    "sku": produto['SKU'],
+                    "idmarca": produto['IdMarca'],
+                    "idproduto": produto['IdProduto'],
+                    "saldo": produto['SaldoAtual'],
+                    "prazo": produto['PrazoProducao']
                 }
                 yield dict_items
             except:
                 print("error")
 
+
 class ReaderExcel:
     grouped = collections.defaultdict(list)
-    
-    def __init__(self,file):
+
+    def __init__(self, file):
         self.file = file
         self.listas = []
 
@@ -181,7 +192,7 @@ class ReaderExcel:
 
                 if next(filter(lambda x: x['sku'] == sku_produto, dicts), None):
                     lista_produto = dicts[0]
-             
+
                     dict_produtos = {}
                     try:
 
@@ -190,13 +201,14 @@ class ReaderExcel:
                         pass
 
                     try:
-                        saldo_produto =  str(rows['SALDO']).replace(".","").replace(",",".")
+                        saldo_produto = str(rows['SALDO']).replace(
+                            ".", "").replace(",", ".")
                         num = float(saldo_produto)
-                        dict_produtos['SALDO'] =  num
+                        dict_produtos['SALDO'] = num
 
                     except:
                         pass
-                        
+
                     try:
                         dict_produtos['PRAZO'] = rows['PRAZO']
 
@@ -224,26 +236,50 @@ class ReaderExcel:
                         pass
 
                     self.listas.append(dict_produtos)
-                
-    def group_by(self, key):  
-        key = lambda key: key['SKU']
 
+    def group_by(self, key):
+        def key(key): return key['SKU']
 
     def get_produtos(self):
         for item in self.listas:
             self.grouped[item['SKU']].append(item)
-
+        lista_saldos = []
         for key, group in self.grouped.items():
+         
             get_prazos(group)
-   
+
             lista_saldos = []
             valores = list(chain(group))
-            produtos_valores = valores[0]
-            if next(filter(lambda x: 'SKU' in produtos_valores, produtos_valores), None):
-                lista_saldos.append(produtos_valores['SALDO'])
+            produtos_valores = valores
+            if next(filter(lambda x: 'SKU' in x, produtos_valores), None):
+                for valores in produtos_valores:
 
-            saldo_produto = sum(lista_saldos)
+                    lista_saldos.append(valores['SALDO'])
+                
+            saldop = float(sum(lista_saldos))
+            produtosomado = {}
+            try:
+                produtosomado['sku'] = valores['SKU']
+            except:
+                produtosomado['sku'] = 'NotFound'
+            try:
+                produtosomado['saldo'] = saldop
+            except:
+                produtosomado['saldo'] = float(0)
+            try:
+                produtosomado['idmarca'] = valores['IDMARCA']
+            except:
+                produtosomado['idmarca'] = 'notfound'
+            try:
+                produtosomado['idproduto'] =  valores['IDPRODUTO']
+            except:
+                produtosomado['idproduto'] = 'notfound'
+            try:
+                produtosomado['saldoanterior'] =  valores['SALDOANTERIOR']
+            except:
+                produtosomado['saldoanterior'] = float(0)
 
-            update_saldo_produtos(sku=produtos_valores['SKU'], saldo = float(saldo_produto)
-                ,idmarca =produtos_valores['IDMARCA'], idproduto = produtos_valores['IDPRODUTO'], saldoanterior = produtos_valores['SALDOANTERIOR'])
-
+   
+              
+            update_saldo_produtos(sku=produtosomado['sku'], saldo=produtosomado['saldo'], idmarca=produtosomado['idmarca'],
+                                  idproduto=produtosomado['idproduto'], saldoanterior=produtosomado['saldoanterior'])
